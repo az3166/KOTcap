ba9d4fabcca7ff5ecbeb41395438257fc4ad16ce
WuKe <wuke2000@msn.com>
2019-04-01 14:57:42 +0800

    wifi update

--- kotcap.ino
+++ kotcap.ino
@@ -1,6 +1,6 @@
 #include "BluetoothSerial.h"
 #include <WiFi.h>
-int servopin=13; 
+int servopin=33; 
 int myangle; 
 int pulsewidth; 
 int target;
@@ -10,7 +10,7 @@ String cmdstring;
 const char *ssid = "KOTCAP_WIFI";
 const char *password = "";
 String header;
-String capstate = "close";
+String capstate = "Close";
 WiFiServer server(80);
 BluetoothSerial BTSerial;
 
@@ -71,59 +71,58 @@ if (cmdstring.length() > 0 )
 
 void webEvent()
 {
-  WiFiClient client = server.available();   
-  if (client) {                            
-    Serial.println("New Client.");         
-    cmdstring = "";               
-    while (client.connected()) {          
-      if (client.available()) {           
-        char c = client.read();          
-        Serial.write(c);                  
+WiFiClient client = server.available();   
+  if (client) {                             
+    while (client.connected()) {   
+      if (client.available()) {   
+        char c = client.read();            
         header += c;
+        if (c == '\n') {    
           if (cmdstring.length() == 0) {
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
             
-            if (header.indexOf("GET /open") >= 0) {
-              Serial.println("Lens cap is open");
-              capstate = "open";
-              target =180;
-            } else if (header.indexOf("GET /close") >= 0) {
-              Serial.println("Lens cap is close");
-              capstate = "close";
-              target =0;
-            }
-
+            if (header.indexOf("GET /180") >= 0) {
+              capstate = "Open 180&deg";
+              target = 180;
+            } else if (header.indexOf("GET /90") >= 0) {
+              capstate = "Open 90&deg";
+              target = 90;
+            } else if (header.indexOf("GET /0") >= 0) {
+              capstate = "Close";
+              target = 0;
+            } 
+            
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 50px 100px;");
-            client.println("text-decoration: none; font-size: 50px; margin: 10px; cursor: pointer;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px; width: 80%; border-radius: 12px;font-size: 24px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
-            
             client.println("<body><h1>KOTCAP</h1>");
-            client.println("<p>Lens cap is " + capstate + ". Press button to </p>");  
-            if (capstate=="close") {
-              client.println("<p><a href=\"/open\"><button class=\"button\">Open</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/close\"><button class=\"button button2\">Close</button></a></p>");
-            } 
+            client.println("<p>Telescope lens cap is " + capstate +".</p>");
+            client.println("<p><a href=\"/180\"> <button class= \"button\">Open 180&deg</button></a></p>");
+            client.println("<p><a href=\"/90\"> <button class=\"button\">Open 90&deg</button></a></p>");
+            client.println("<p><a href=\"/0\"> <button class=\"button button2\">Close</button></a></p>");
+            client.println("</body></html>");
+
             client.println();
             break;
           } else { 
             cmdstring = "";
           }
         } else if (c != '\r') {  
-          cmdstring += c;    
+          cmdstring += c;     
         }
       }
     }
     header = "";
     client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }
 
